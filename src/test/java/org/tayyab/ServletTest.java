64b21799611cb4a50eb73150ad7f037d763a2ad9
itayyab0o1 <tayyab.ismail22@gmail.com>
2022-03-26 14:13:10 +0500

    test

--- src/test/java/org/tayyab/ServletTest.java
+++ src/test/java/org/tayyab/ServletTest.java
@@ -8,18 +8,48 @@ import com.mockrunner.mock.web.WebMockObjectFactory;
 import com.mockrunner.servlet.ServletTestModule;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class ServletTest{
     private ServletTestModule tester;
     private WebMockObjectFactory factory;
     static String id="0";
+    public static final DockerImageName MYSQL_80_IMAGE = DockerImageName.parse("mysql:8.0.24");
     @Before
     public void setup() {
         factory = new WebMockObjectFactory();
         tester = new ServletTestModule(factory);
+
     }
-   /* @Test
+    @BeforeClass
+    public static void initDb(){
+        try (MySQLContainer<?> mysqlOldVersion = new MySQLContainer<>(MYSQL_80_IMAGE)
+                .withDatabaseName("jersey_db_test")
+                .withUsername("root")
+                .withPassword("root")
+                .withEnv("MYSQL_ROOT_PASSWORD", "root")
+                .withInitScript("jersey.sql")
+//                .withExposedPorts(34343)
+//                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+//                        new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(34343), new ExposedPort(3306)))
+//                ));
+//                .withConfigurationOverride("somepath/mysql_conf_override")
+             //  .withLogConsumer(new Slf4jLogConsumer(logger))
+        )
+        {
+
+            mysqlOldVersion.start();
+        }catch (Exception ex){
+          ex.printStackTrace();
+        }
+    }
+
+    @Test
     public void doPostAddTest() {
         tester.addRequestParameter("action", "no");
         tester.addRequestParameter("personsname","Person from Servlet post test");
@@ -84,6 +114,5 @@ public class ServletTest{
     // assertion: status code should be 200
         Assert.assertEquals("Data","Data deleted Successfully", factory.getMockResponse().getHeader("msg"));
     }
-*/
 }
 
